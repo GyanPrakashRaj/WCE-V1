fec7dd566d2da3844efa8708e7d513a127432838
GyanPrakashRaj <thecornerstandingman@gmail.com>
2022-11-14 20:05:31 +0530

    s21

--- components/cta.js
+++ components/cta.js
@@ -7,10 +7,10 @@ export default function Cta() {
       <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">
-            Ready to try-out this template?
+            JOIN OUR NEW EVENT
           </h2>
           <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
-            Don't let your visitors see a poor landing.
+            Don't let your miss this platform out.
           </p>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
@@ -19,7 +19,7 @@ export default function Cta() {
             target="_blank"
             rel="noopener"
             className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 ">
-            Download for Free
+            REGISTER NOW
           </a>
         </div>
       </div>
